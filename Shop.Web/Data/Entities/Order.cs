13ac80b851dae3048fca314e44b4c7997b455b0b
Daniel Giraldo Sepulveda <danielgiraldo92@gmail.com>
2019-03-24 16:28:35 -0500

    Modal and Date Picker
    Modal and Date Picker

--- Shop.Web/Data/Entities/Order.cs
+++ Shop.Web/Data/Entities/Order.cs
@@ -11,11 +11,11 @@
 
         [Required]
         [Display(Name = "Order date")]
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}", ApplyFormatInEditMode = false)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy hh:mm tt}", ApplyFormatInEditMode = false)]
         public DateTime OrderDate { get; set; }
 
         [Display(Name = "Delivery date")]
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}", ApplyFormatInEditMode = false)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy hh:mm tt}", ApplyFormatInEditMode = false)]
         public DateTime? DeliveryDate { get; set; }
 
         [Required]
@@ -33,7 +33,7 @@
         public decimal Value { get { return this.Items == null ? 0 : this.Items.Sum(i => i.Value); } }
 
         [Display(Name = "Order date")]
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}", ApplyFormatInEditMode = false)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy hh:mm tt}", ApplyFormatInEditMode = false)]
         public DateTime? OrderDateLocal
         {
             get
