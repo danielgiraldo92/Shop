670ec2f80da3421ed0b8d19f628a9b0b34e16698
Daniel Giraldo Sepulveda <danielgiraldo92@gmail.com>
2019-03-25 09:50:20 -0500

    Consume token from app
    Consume token from app

--- Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -2,11 +2,29 @@
 {
     using System.Windows.Input;
     using GalaSoft.MvvmLight.Command;
+    using Shop.Common.Models;
+    using Shop.Common.Services;
     using Shop.UIForms.Views;
     using Xamarin.Forms;
 
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
+        private readonly ApiService apiService;
+        private bool isRunning;
+        private bool isEnabled;
+
+        public bool IsRunning
+        {
+            get => this.isRunning;
+            set => this.SetValue(ref this.isRunning, value);
+        }
+
+        public bool IsEnabled
+        {
+            get => this.isEnabled;
+            set => this.SetValue(ref this.isEnabled, value);
+        }
+
         public string Email { get; set; }
 
         public string Password { get; set; }
@@ -15,7 +33,9 @@
 
         public LoginViewModel()
         {
+            this.apiService = new ApiService();
             this.Email = "danielgiraldo92@gmail.com";
+            this.IsEnabled = true;
             this.Password = "123456";
         }
 
@@ -39,21 +59,38 @@
                 return;
             }
 
-            if (!this.Email.Equals("danielgiraldo92@gmail.com") || !this.Password.Equals("123456"))
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            var request = new TokenRequest
+            {
+                Password = this.Password,
+                Username = this.Email
+            };
+
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+            var response = await this.apiService.GetTokenAsync(
+                url,
+                "/Account",
+                "/CreateToken",
+                request);
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            if (!response.IsSuccess)
             {
                 await Application.Current.MainPage.DisplayAlert(
                     "Error", 
-                    "User or password wrong.", 
+                    "Email or password incorrect.",
                     "Accept");
                 return;
             }
 
-            //await Application.Current.MainPage.DisplayAlert(
-            //  "Ok", 
-            //  "Fuck yeah!!!", 
-            //  "Accept");
-
-            MainViewModel.GetInstance().Products = new ProductsViewModel();
+            var token = (TokenResponse)response.Result;
+            var mainViewModel = MainViewModel.GetInstance();
+            mainViewModel.Token = token;
+            mainViewModel.Products = new ProductsViewModel();
             await Application.Current.MainPage.Navigation.PushAsync(new ProductsPage());
         }
     }
